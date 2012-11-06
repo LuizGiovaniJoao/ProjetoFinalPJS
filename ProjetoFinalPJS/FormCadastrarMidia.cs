87adc39569b1bb9473057a8ac595da764a411dda
Joao Cicero de Lira Filho <jc_ifsp@hotmail.com>
2012-11-06 20:31:11 -0200

    alteracao formulario cadastro midia

--- ProjetoFinalPJS/FormCadastrarMidia.cs
+++ ProjetoFinalPJS/FormCadastrarMidia.cs
@@ -20,5 +20,59 @@ namespace ProjetoFinalPJS
         {
 
         }
+
+        private void checkBox_album_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox_album.Checked)
+            {
+                checkBox_musica.Checked = false;
+                textBoxInterprete.Enabled = true;
+                textBoxAutor.Enabled = true;
+                textBoxAlbum.Enabled = true;
+                dateTimePickerAlbum.Enabled = true;
+                dateTimePickerCompra.Enabled = true;
+                textBoxOrigemCompra.Enabled = true;
+                comboBoxMidia.Enabled = true;
+                comboBoxNota.Enabled = true;
+                textBoxObsevacoes.Enabled = true;
+            }
+            else
+            {
+                textBoxInterprete.Enabled = false;
+                textBoxAutor.Enabled = false;
+                textBoxAlbum.Enabled = false;
+                dateTimePickerAlbum.Enabled = false;
+                dateTimePickerCompra.Enabled = false;
+                textBoxOrigemCompra.Enabled = false;
+                comboBoxMidia.Enabled = false;
+                comboBoxNota.Enabled = false;
+                textBoxObsevacoes.Enabled = false;
+            }
+        }
+
+        private void checkBox_musica_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox_musica.Checked)
+            {
+                checkBox_album.Checked = false;
+                textBoxInterprete.Enabled = true;
+                textBoxAutor.Enabled = true;
+                dateTimePickerCompra.Enabled = true;
+                textBoxOrigemCompra.Enabled = true;
+                comboBoxMidia.Enabled = true;
+                comboBoxNota.Enabled = true;
+                textBoxObsevacoes.Enabled = true;
+            }
+            else 
+            {
+                textBoxInterprete.Enabled = false;
+                textBoxAutor.Enabled = false;
+                dateTimePickerCompra.Enabled = false;
+                textBoxOrigemCompra.Enabled = false;
+                comboBoxMidia.Enabled = false;
+                comboBoxNota.Enabled = false;
+                textBoxObsevacoes.Enabled = false; 
+            }
+        }
     }
 }
